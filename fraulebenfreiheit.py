4bbdaf2e6551e3b798ecf26dba54bde508ccc961
Betisa <betisa@Air-von-Betisa.fritz.box>
2022-10-25 15:49:18 +0200

    first commit

---
+++ fraulebenfreiheit.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.header(":mailbox: Schreibe deinen Abgeordneten")
+
+st.write("Seit Wochen weht im Iran ein Wind der Veränderungen. Und seit Wochen lehnen sich immer mehr gesellschaftliche Schichten gegen das Unrechtsregime im Iran.")
+
