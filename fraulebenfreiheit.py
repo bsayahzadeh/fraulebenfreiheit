d1ac5fabf8dce61d5013fe4ab4a075b2c43db2e7
bsayahzadeh <110176468+bsayahzadeh@users.noreply.github.com>
2022-10-25 15:57:56 +0200

    Update fraulebenfreiheit.py

--- fraulebenfreiheit.py
+++ fraulebenfreiheit.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+df = pd.read_csv("/Users/betisa/Desktop/fraulebenfreiheit/deputies.csv", sep=";")
+
 st.header(":mailbox: Schreibe deinen Abgeordneten")
 
 st.write("Seit Wochen weht im Iran ein Wind der Veränderungen. Und seit Wochen lehnen sich immer mehr gesellschaftliche Schichten gegen das Unrechtsregime im Iran.")
