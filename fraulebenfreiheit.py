13ddbcc355637f9d954108dcb58bdfb310575289
bsayahzadeh <110176468+bsayahzadeh@users.noreply.github.com>
2022-10-25 18:42:03 +0200

    Insert function for plz

--- fraulebenfreiheit.py
+++ fraulebenfreiheit.py
@@ -4,13 +4,18 @@ import numpy as np
 from pathlib import Path
 
 path= Path.cwd() / "deputies.csv"
-path
 
 df = pd.read_csv(path, sep=";")
 
 st.header(":mailbox: Schreibe deinen Abgeordneten")
 
 st.write("Seit Wochen weht im Iran ein Wind der Veränderungen. Und seit Wochen lehnen sich immer mehr gesellschaftliche Schichten gegen das Unrechtsregime im Iran.")
+
+@st.cache(suppress_st_warning= True, allow_output_mutation=True)
+def get_info(plz):
+    info = df[["Abgeordnete", "E-Mail", "Partei", "Wahlkreis"]].loc[df.PLZ.str.contains(plz)]
+    info = info.set_index("Abgeordnete").sort_values(by=["Wahlkreis"])
+    return info
 
 with st.form("mail"):
     name = st.text_input("Vollständiger Name")
